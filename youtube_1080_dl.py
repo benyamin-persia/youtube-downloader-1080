00180907000fe49a8b22e435414287e7814b5a15
benyamin-persia <35182026+benyamin-persia@users.noreply.github.com>
2024-12-02 22:46:29 -0500

    Update youtube_1080_dl.py

--- youtube_1080_dl.py
+++ youtube_1080_dl.py
@@ -1,10 +1,11 @@
-import yt_dlp 
+import yt_dlp # pip install yt-dlp
+
 def download_video(link):
     ydl_opts = {
         'format': 'bestvideo+bestaudio',
         'outtmpl': '%(title)s.%(ext)s',
         'merge_output_format': 'mp4',
-        'ffmpeg_location': 'C:/ffmpeg/bin',  # Replace with the actual path
+        'ffmpeg_location': 'C:/ffmpeg-master-latest-win64-gpl-shared/bin',  # unzip this "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-gpl-shared.zip"  in c:
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([link])
